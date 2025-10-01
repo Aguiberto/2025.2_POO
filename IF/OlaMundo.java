d51f8573991f55283e032a577812e80cebf56080
Aguiberto <aguibertocandido@gmail.com>
2025-10-01 13:30:27 +0000

    primeiros

---
+++ IF/OlaMundo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class OlaMundo{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual o seu nome? ");
+        String nome = sc.nextLine();
+        System.out.println("Olá " + nome + "!");
+        System.out.print("Em que ano você nasceu: ");
+        
+
+    } 
+}
