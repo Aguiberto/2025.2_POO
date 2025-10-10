57e7c2ca5128152e271127dc82f42a843d842b96
Aguiberto <aguibertocandido@gmail.com>
2025-10-10 13:43:31 +0000

    +1

---
+++ IF/LISTA2/Sequencia1.java
@@ -0,0 +1,14 @@
+public class Sequencia1{
+    public static void main(String[] args) {
+        
+        int j = 60;
+        int i = 0;
+
+        while( j > 0){
+            j -= 5;
+            i +=1;
+        System.out.println("I" + "=" + "i" +" "+ "J" + "=" + j);
+        }
+
+    }
+}
