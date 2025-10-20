272c1e134f455616fd5ae7e684ace58a5b6ed209
Aguiberto <aguibertocandido@gmail.com>
2025-10-20 11:27:49 +0000

    fim

--- IF/LISTA2/Dama.java
+++ IF/LISTA2/Dama.java
@@ -11,8 +11,39 @@ public class Dama{
         int x2 = scanner.nextInt();
         int y2 = scanner.nextInt();
 
-        if()
+        int mov = 0;
+
+        while( x1 != 0 || x2 != 0 || y1 != 0 || y2 != 0){
+
+            // pontos estão no mesmo lugar
+            if(x1 == x2 && y1 == y2){
+                mov = 0;
+
+            // pontos alinhados verticalmente ou horizontalmente
+            }else if(y1 == y2 || x1 == x2){
+                mov = 1;
+
+            // pontos alinhados na diagonal
+            }else if(Math.abs((x1 - x2)) == Math.abs((y1 - y2))){
+                mov =1;
+
+            // pontos não alinhados
+            }else{
+                mov = 2;
+            }
+
+            System.out.println(mov);
+
+            x1 = scanner.nextInt();
+            y1 = scanner.nextInt();
+            x2 = scanner.nextInt();
+            y2 = scanner.nextInt();
+
+        }
 
         scanner.close();
+
+        System.exit(0);
+         
     }
 }
