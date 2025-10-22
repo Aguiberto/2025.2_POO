5dcdd74184eb4091784401b247c5ee8490fb141b
Aguiberto <aguibertocandido@gmail.com>
2025-10-22 12:39:44 +0000

    +1

---
+++ IF/LISTA3/Led.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Led{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int casos = scanner.nextInt();
+        
+
+    }
+}
