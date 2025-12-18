0d458417e676036814ebfea15f7ed689fbe4ba53
unknown <aguibertocandido@gmail.com>
2025-12-18 15:20:32 -0300

    + classes

---
+++ IF/LISTA6_HERANCAS/Construtora.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+
+public class Construtora{
+
+    private LinkedList<Funcionario> funcionarios;
+
+    public Construtora(){
+        funcionarios = new LinkedList<>();
+    }
+
+    public void Inserir(Funcionario novo_funcionario){
+        funcionarios.add(novo_funcionario);
+    }
+
+    public LinkedList<Funcionario> Colaboradores(){
+        return funcionarios;
+    }
+
+    public LinkedList<Engenheiro> getEngenheiros(){
+        LinkedList<Engenheiro> lista_engenheiro = new LinkedList<>();
+
+            for(Funcionario f: funcionarios){
+                if( f instanceof Engenheiro){
+                    lista_engenheiro.add((Engenheiro)f);
+                }
+            }
+            return lista_engenheiro;
+    }
+
+    public LinkedList<Motorista> getMotorista(){
+        LinkedList<Motorista> lista_motorista = new LinkedList<>();
+
+        for(Funcionario f: funcionarios){
+            if(f instanceof Motorista){
+                lista_motorista.add((Motorista)f);
+            }
+        } return lista_motorista;
+    }
+
+}
---
+++ IF/LISTA6_HERANCAS/testeContrutora.java
@@ -0,0 +1,9 @@
+public class testeConstrutora{
+    public static void main(String[] args){
+
+        Construtora afilho = new Construtora();
+        Funcionario  a = new Funcionario();
+
+
+    }
+}
